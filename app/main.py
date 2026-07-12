f590fc87a82967d11f7f17094d99926c94ab0614
Yash-KK <reachyashk@gmail.com>
2026-07-12 23:07:17 +0530

    setup fastapi-soo

--- app/main.py
+++ app/main.py
@@ -6,15 +6,46 @@ from fastapi import FastAPI
 from app.api.v1 import upload
 from app.core.config import PROJECT_ROOT
 
+import os
+from fastapi import Request
+from fastapi_sso.sso.google import GoogleSSO
+
+load_dotenv()
+CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
     load_dotenv(PROJECT_ROOT / ".env")
     yield
 
+sso = GoogleSSO(
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    redirect_uri="http://localhost:8000/auth/callback",
+    allow_insecure_http=True,
+)
 
 app = FastAPI(title="Paper Craft", lifespan=lifespan)
 
+
+
+
+
+
+@app.get("/auth/login")
+async def auth_init():
+    """Initialize auth and redirect"""
+    async with sso:
+        return await sso.get_login_redirect(params={"prompt": "consent", "access_type": "offline"})
+
+
+@app.get("/auth/callback")
+async def auth_callback(request: Request):
+    """Verify login"""
+    async with sso:
+        user = await sso.verify_and_process(request)
+    return user
 app.include_router(upload.router, prefix="/api/v1")
 
 
