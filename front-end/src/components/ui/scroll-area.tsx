40ee7c38906555f81b4fbba5179dbbef220b41e6
Yash-KK <reachyashk@gmail.com>
2026-07-23 21:46:29 +0530

    scrollbar updates

--- front-end/src/components/ui/scroll-area.tsx
+++ front-end/src/components/ui/scroll-area.tsx
@@ -36,14 +36,14 @@ function ScrollBar({
       data-orientation={orientation}
       orientation={orientation}
       className={cn(
-        "flex touch-none p-px transition-colors select-none data-horizontal:h-2.5 data-horizontal:flex-col data-horizontal:border-t data-horizontal:border-t-transparent data-vertical:h-full data-vertical:w-2.5 data-vertical:border-l data-vertical:border-l-transparent",
+        "flex cursor-pointer touch-none p-px transition-colors select-none data-horizontal:h-2.5 data-horizontal:flex-col data-horizontal:border-t data-horizontal:border-t-transparent data-vertical:h-full data-vertical:w-2.5 data-vertical:border-l data-vertical:border-l-transparent",
         className
       )}
       {...props}
     >
       <ScrollAreaPrimitive.Thumb
         data-slot="scroll-area-thumb"
-        className="relative flex-1 rounded-full bg-border"
+        className="relative flex-1 cursor-pointer rounded-full bg-slate-500 dark:bg-gray-500"
       />
     </ScrollAreaPrimitive.Scrollbar>
   )
